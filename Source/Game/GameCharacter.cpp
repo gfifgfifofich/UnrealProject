6f5e6abd84912b7b4353db97ed89c2b4d26ba9ac
Gfif <gfif90000@gmail.com>
2023-11-01 17:45:50 +0300

    pog

--- Source/Game/GameCharacter.cpp
+++ Source/Game/GameCharacter.cpp
@@ -23,7 +23,7 @@ AGameCharacter::AGameCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
-	GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 64.f, 0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
