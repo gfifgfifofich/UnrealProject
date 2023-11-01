ed8311d719ce73e60e3e181a560e3b0077204e44
Gfif <gfif90000@gmail.com>
2023-11-01 17:21:27 +0300

    TestCharacterStuff

--- Source/Unrealproject/Unrealproject.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Unrealproject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unrealproject, "Unrealproject" );
-
-DEFINE_LOG_CATEGORY(LogUnrealproject)
- 
