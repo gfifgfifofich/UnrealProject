4764b70ea05d420befa428f83a510a70ffc47424
Gfif <gfif90000@gmail.com>
2023-11-03 18:27:52 +0300

    stuff

--- Source/Game/Private/TestEnemyController.cpp
+++ Source/Game/Private/TestEnemyController.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "TestEnemyController.h"
 
 #include "Blueprint/AIBlueprintHelperLibrary.h"
-#include "TestEnemyController.h"
 
 #include "Kismet/GameplayStatics.h"
 #include "../GameCharacter.h"
